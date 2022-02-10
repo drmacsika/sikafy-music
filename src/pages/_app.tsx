45d0dd7c19fef4c1e3e218a1e3c38935a6452e01
drmacsika <drmacsika@gmail.com>
2022-02-10 22:00:11 +0100

    Add NextAuth Session provider.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../../styles/globals.css'
+import 'tailwindcss/tailwind.css';
+import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+  return (
+    <SessionProvider>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
