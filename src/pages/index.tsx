a14c75132bf92231024972e80125226a9385f271
drmacsika <drmacsika@gmail.com>
2022-02-10 09:46:10 +0100

    Add SideBar Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,27 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import React from 'react';
+import SideBar from '../components/SideBar';
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-center py-2">
         <Head>
-          <title>Create Next App</title>
+          <title>Sikafy Music: Music on the fly.</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-          <h1 className="text-6xl font-bold">
-            Hello From{' '}
-            <a className="text-blue-600" href="https://nextjs.org">
-              Sikafy Music!
-            </a>
-          </h1>
+        <main className="flex w-full flex-1">
+          <SideBar />
+          
         </main>
 
-        <footer className="flex h-24 w-full items-center justify-center border-t">
-          <a
-            className="flex items-center justify-center"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-          </a>
+        <footer className="flex">
+          
         </footer>
       </div>
     </>
-  )
+  );
 }
+
+export default Home;
