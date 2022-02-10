fd88c2cb5cc84d40c4729eb1d7dd49e91923b7f1
drmacsika <drmacsika@gmail.com>
2022-02-10 22:00:59 +0100

    Finalize the Next Auths authentication function.

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { LOGIN_URL } from "../../../lib/spotify"
+import refreshAccessToken from "../../../lib/jwt-refresh-access-token"
 
 export default NextAuth({
   providers: [
