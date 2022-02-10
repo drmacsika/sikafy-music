a271a94165de70179c16f4b564c8f8a6582f7543
drmacsika <drmacsika@gmail.com>
2022-02-10 16:23:22 +0100

    Add JWT function, refresh access token, authorization and session.

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,11 +1,46 @@
-import NextAuth from "next-auth"
-import SpotifyProvider from "next-auth/providers/spotify"
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+import { LOGIN_URL } from "../../../lib/spotify"
 
 export default NextAuth({
   providers: [
     SpotifyProvider({
       clientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
-    })
+      authorization: LOGIN_URL,
+    }),
   ],
-})
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login'
+  },
+  callbacks: {
+    async jwt({token, account, user}){
+      // first Sign in
+      if (account && user){
+        return {
+          ...token,
+          username: account.providerAccountId,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          accessTokenExpires: account.expires_at * 1000,
+        }
+      }
+      
+      // Check if initial token has not expired
+      // return the token
+      if (Date.now() > token.accessTokenExpires){
+        return token;
+      }
+
+      // Refresh the access token to get a new token
+      return await refreshAccessToken(token);
+    },
+    async session({ session, token}) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+      return session;
+    }
+  },
+});
