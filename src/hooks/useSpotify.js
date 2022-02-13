1af0c5dfa8613f49abfb3496124b00847fb9ac54
drmacsika <drmacsika@gmail.com>
2022-02-13 20:36:38 +0100

    Add custom hook.

---
+++ src/hooks/useSpotify.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+import spotifyApi from '../lib/spotify';
+
+const useSpotify = () => {
+    const { data: session, status } = useSession();
+
+    useEffect(() => {
+        if (session){
+            if ( session.error === 'RefreshAccessTokenError'){
+                signIn();
+            }
+            spotifyApi.setAccessToken(session?.user?.accessToken)
+        }
+    }, [session]);
+
+    return null;
+}
+
+export default useSpotify;
