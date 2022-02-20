8900961434fab4a17c24242fbc467c85c1d85310
drmacsika <drmacsika@gmail.com>
2022-02-20 14:43:33 +0100

    Add recoil state manager.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import 'tailwindcss/tailwind.css';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from "recoil"
 
 function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
     <SessionProvider>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   )
 }
