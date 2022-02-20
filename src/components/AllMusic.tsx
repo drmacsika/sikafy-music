7889c242918f353c511772ca3bd13483330e9e83
drmacsika <drmacsika@gmail.com>
2022-02-20 17:28:54 +0100

    All AllMusic component.

---
+++ src/components/AllMusic.tsx
@@ -0,0 +1,17 @@
+import { useRecoilState } from "recoil";
+import { playlistState } from "../atoms/playlistAtom";
+import Song from "./Song";
+
+function Songs() {
+  const [playlist, setPlaylist] = useRecoilState(playlistState);
+
+  return (
+    <div className="px-8 flex flex-col space-y-1 pb-28">
+      {playlist?.tracks?.items.map((track, i) => (
+        <Song key={track.track.id} track={track} order={i} />
+      ))}
+    </div>
+  );
+}
+
+export default Songs;
